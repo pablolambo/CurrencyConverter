5c7f39a52fb5401365a05742700a67b4d87474d9
Paul Frank <pawelfrankowski42@gmail.com>
2024-11-04 19:03:08 +0100

    added fetch Currency names

--- src/currency_converter/lib/exchange_rate_service.dart
+++ src/currency_converter/lib/exchange_rate_service.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 
 class ExchangeRateService {
   final String apiUrl = 'https://api.exchangerate-api.com/v4/latest/';
-  final List<String> targetCurrencies = ['PLN', 'GBP', 'EUR', 'USD'];
 
   Future<Map<String, dynamic>> fetchExchangeRates(String baseCurrency) async {
     try {
@@ -13,18 +12,33 @@ class ExchangeRateService {
         final data = json.decode(response.body);
         final rates = data['rates'] as Map<String, dynamic>;
 
-        final filteredRates = Map.fromEntries(
-          rates.entries
-              .map((entry) => MapEntry(entry.key, entry.value.toDouble()))
-              .where((entry) => targetCurrencies.contains(entry.key)),
-        );
-
-        return filteredRates;
+         return Map.fromEntries(
+            rates.entries
+                .map((entry) => MapEntry(entry.key, entry.value.toDouble()))
+                .where((entry) => entry.key != baseCurrency)
+         );
       } else {
         throw Exception('Failed to load exchange rates');
       }
     } catch (e) {
       throw Exception('Error fetching exchange rates: $e');
+    }
+  }
+
+   Future<List<String>> fetchCurrencyNames(String baseCurrency) async {
+    try {
+      final response = await http.get(Uri.parse('$apiUrl$baseCurrency'));
+      
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final rates = data['rates'] as Map<String, dynamic>;
+
+        return rates.keys.toList();
+      } else {
+        throw Exception('Failed to load currency names');
+      }
+    } catch (e) {
+      throw Exception('Error fetching currency names: $e');
     }
   }
 }
